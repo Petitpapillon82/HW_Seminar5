b27a99c7a6aba8e07240908a857035280c1f9263
Irina <petitpapillon82@gmail.com>
2022-09-04 20:21:27 +0400

    Add T2

---
+++ T2/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+Console.Clear();
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int [] array = GetArray(size, -100, 101);
+Console.WriteLine("Массив: ");
+PrintArray(array);
+int sum = 0;
+for (int j = 0; j < size; j+=2)
+    sum = sum + array[j];
+    Console.WriteLine($"В массиве {size} чисел, сумма элементов на нечётных позициях равна {sum}");
+
+int [] GetArray(int size, int min, int max){
+    int[] result = new int[size];
+
+    for(int i = 0; i < size; i++){
+        result[i] = new Random().Next(min, max + 1);
+    }
+return result;
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
